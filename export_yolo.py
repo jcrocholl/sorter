0e857d68757880d321569b99d38e2b16b53518fd
Johann Rocholl <johann@rocholl.net>
2023-09-19 19:42:34 -0700

    Count the number of train/val/test images per class.

--- export_yolo.py
+++ export_yolo.py
@@ -2,6 +2,7 @@
 
 """Exports training/evaluation/test datasets to YOLO format."""
 
+from collections import defaultdict
 import os
 import pathlib
 import re
@@ -55,27 +56,42 @@ def find_paths_with_jpg_files(path: pathlib.Path) -> list[pathlib.Path]:
     return results
 
 
-def write_yaml(class_names: list[str]):
+def write_yaml(
+    paths: list[pathlib.Path],
+    num_train_per_class: dict[int, int],
+    num_val_per_class: dict[int, int],
+    num_test_per_class: dict[int, int],
+):
     """Writes dataset config file in YAML format for YOLO."""
+    class_names = [path_to_class(path) for path in paths]
+    longest = max(len(class_name) for class_name in class_names)
     with open("../yolov7/data/bricks.yaml", "wt", encoding="utf-8") as outfile:
         print("# Bricks dataset by Johann C. Rocholl", file=outfile)
         print("train: bricks/images/train2023", file=outfile)
         print("val: bricks/images/val2023", file=outfile)
         print("test: bricks/images/test2023", file=outfile)
-        print(f"nc: {len(class_names)}", file=outfile)
+        print(f"nc: {len(paths)}", file=outfile)
         print("names: [", file=outfile)
-        for name in class_names:
-            print(f"    {name},", file=outfile)
+        for class_id, class_name in enumerate(class_names):
+            num_train = num_train_per_class[class_id]
+            num_val = num_val_per_class[class_id]
+            num_test = num_test_per_class[class_id]
+            ljust = " " * (longest - len(class_name))
+            print(
+                f"    {class_name},{ljust}  # train={num_train:<4} val={num_val:<3} test={num_test}",
+                file=outfile,
+            )
         print("]", file=outfile)
 
 
 def export_dir(
     path: pathlib.Path,
     class_id: int,
-):
+) -> tuple[int, int, int]:
     """Exports one class (directory) of images to YOLO format."""
     class_name = path_to_class(path)
     print(f"id={class_id}\tname={class_name}\tfrom {path}")
+    num_train = num_val = num_test = 0
     for child in path.iterdir():
         if not child.name.endswith(".jpg"):
             continue
@@ -98,6 +114,12 @@ def export_dir(
         fh = (b - t) / h
         output_base = f"{base}_{class_name}"
         split = train_test_split(base)
+        if "train" in split:
+            num_train += 1
+        elif "val" in split:
+            num_val += 1
+        elif "test" in split:
+            num_test += 1
 
         images = pathlib.Path("../yolov7/bricks/images") / split
         output_jpg = images / f"{output_base}.jpg"
@@ -110,6 +132,7 @@ def export_dir(
         output_txt = labels / f"{output_base}.txt"
         with open(output_txt, "wt", encoding="utf-8") as txt:
             txt.write(f"{class_id:d} {fx:.3f} {fy:.3f} {fw:.3f} {fh:.3f}\n")
+    return num_train, num_val, num_test
 
 
 def main():
@@ -117,9 +140,15 @@ def main():
     here = pathlib.Path(".")
     paths = find_paths_with_jpg_files(here)
     paths.sort()
-    write_yaml([path_to_class(path) for path in paths])
-    for index, path in enumerate(paths):
-        export_dir(path, index)
+    num_train_per_class = defaultdict(int)
+    num_val_per_class = defaultdict(int)
+    num_test_per_class = defaultdict(int)
+    for class_id, path in enumerate(paths):
+        num_train, num_val, num_test = export_dir(path, class_id)
+        num_train_per_class[class_id] = num_train
+        num_val_per_class[class_id] = num_val
+        num_test_per_class[class_id] = num_test
+    write_yaml(paths, num_train_per_class, num_val_per_class, num_test_per_class)
 
 
 if __name__ == "__main__":
